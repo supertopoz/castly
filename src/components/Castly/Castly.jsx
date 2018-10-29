7c25b061141f4fe9c73a6a447740121aa180c291
supertopoz <deepsleepsounds@gmail.com>
2018-10-29 13:43:05 +0900

    add info graphic

--- src/components/Castly/Castly.jsx
+++ src/components/Castly/Castly.jsx
@@ -32,6 +32,32 @@ const HiddenCanvas = styled.canvas`
    display: none;
 `
 
+const InfoGraphics = styled.div`
+  display:grid;
+  grid-gap: 20px;
+  border: 1px lightgrey solid;
+  border-radius: 10px;
+  box-shadow: 1px 1px 5px 1px lightgrey;
+  padding: 20px;
+`
+
+const Header = styled.div`
+  display:grid;
+  color: #6b6b6b;
+  font-size: 2em;
+  text-align: center; 
+`
+
+const Image = styled.img`
+  margin: 0 auto;
+`
+
+const Info = styled.div`
+  text-align: center;
+  font-size: 1.3em
+  color: #6b6b6b;
+`
+
 
 class Castly extends React.Component {
 
@@ -40,7 +66,11 @@ class Castly extends React.Component {
     let displayCanvas = <DisplayCanvas/>
     let displayVideo = <Video />
     if(this.props.castly.images.length === 0){
-      display = <div>Add picture files of a PDF document</div> 
+      display = <InfoGraphics>
+      <Header>Simple Tool for Screen Casting Presentations</Header>
+      <Info>Don't forget all popular presentation tools export to PDF.</Info>
+      <Image src={loading.uploadDiagram()}/>
+      </InfoGraphics> 
       displayCanvas = <div></div>
       displayVideo = <div></div>
     }
